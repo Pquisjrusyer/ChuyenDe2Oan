5145fb128faf605ebd8e063b94099a96641066b3
Pquine1502 <hpq.graphicdesign.afitel.15022003@gmail.com>
2026-08-19 10:05:30 +0700

    fix production image loading on login-success and auth pages by inlining SVG vectors directly and adding graceful fallbacks

--- js/pages/check-inbox.js
+++ js/pages/check-inbox.js
@@ -12,7 +12,7 @@ export async function renderCheckInbox(container) {
       
       <!-- Background Grunge Overlay (1316:81062) -->
       <div class="reg-success-grunge-bg" data-node-id="1316:81062">
-        <img src="./assets/7c68e3ca72278b8193b62bcefa0dbacd65ab30a8.png" alt="" />
+        <img src="./assets/7c68e3ca72278b8193b62bcefa0dbacd65ab30a8.png" alt="" onerror="this.style.display='none'" />
       </div>
 
       <!-- Main Content Card (1316:81056) -->
@@ -23,9 +23,11 @@ export async function renderCheckInbox(container) {
           KIỂM TRA HỘP THƯ CỦA BẠN
         </h1>
 
-        <!-- Golden Shield Check Icon (1316:81377) -->
-        <div class="reg-success-shield-icon" data-node-id="1316:81377">
-          <img src="./assets/c3bbd3c027cc168dec7fa3fcb244af583a85d9fa.svg" alt="Kiểm tra hộp thư" />
+        <!-- Golden Shield Check Icon (1316:81377) - Inline SVG for 100% Production Reliability -->
+        <div class="reg-success-shield-icon" data-node-id="1316:81377" aria-label="Kiểm tra hộp thư">
+          <svg preserveAspectRatio="xMidYMid meet" width="100%" height="100%" viewBox="0 0 252 252" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M113.592 36.8154C121.515 33.4196 130.485 33.4197 138.408 36.8154L193.136 60.2705C196.996 61.9251 199.5 65.7207 199.5 69.9209V125.076C199.5 145.422 189.674 164.516 173.118 176.342L138.206 201.279C130.904 206.495 121.096 206.495 113.794 201.279L78.8818 176.342C62.3257 164.516 52.5 145.422 52.5 125.076V69.9209C52.5001 65.7207 55.0037 61.925 58.8643 60.2705L113.592 36.8154ZM179.165 73.2549C174.197 69.2978 166.962 70.1171 163.005 75.085L109.712 141.991L88.1494 120.336C83.6681 115.835 76.3865 115.82 71.8857 120.301C67.3851 124.782 67.3694 132.064 71.8506 136.564L102.121 166.967C107.182 172.05 115.543 171.587 120.012 165.977L180.995 89.415C184.952 84.4472 184.133 77.212 179.165 73.2549Z" fill="#9C7E21"/>
+          </svg>
         </div>
 
         <!-- Description Note (1316:81058 & 1316:81367) -->
@@ -40,28 +42,39 @@ export async function renderCheckInbox(container) {
 
         <!-- CTA Button 1: Quay lại đăng nhập (1316:81060) -->
         <div class="reg-success-btn-frame" data-node-id="1316:81060">
-          <!-- Horror Metal Side Brackets -->
+          <!-- Horror Metal Side Bracket Left -->
           <div class="reg-success-bracket-left">
-            <img src="./assets/5330b79b9e3ec9f5d8e099674f3b9cd251731b0b.svg" alt="" />
+            <svg width="100%" height="100%" viewBox="0 0 576.28 72.597" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M14.054 39.7269C10.3338 38.723 4.96024 38.1493 0.826722 38.0059V35.1375C21.081 34.1336 25.1409 25.7705 26.7943 19.6035C27.9209 15.0431 17.5428 0.35201 45.95 0H56.2838C118.7 0.143418 181.116 -4.38094e-06 243.532 0.286833C341.083 0.573669 479.969 0.717092 576.28 1.43418C520.891 1.72102 465.502 1.86444 409.7 2.15128C298.508 2.43811 155.488 2.72496 45.5366 2.72496C24.7332 2.72496 49.4663 27.6035 21.0809 34.4204C15.7074 36.2849 8.68039 37.8625 0 38.0059V35.1375C9.5187 35.384 16.9866 36.7825 22.6721 38.8382C25.1301 39.7269 16.4888 40.384 14.054 39.7269Z" fill="#9C7E21"/>
+              <path d="M14.054 32.8701C10.3338 33.8741 4.96024 34.4477 0.826722 34.5911V37.4595C21.081 38.4634 25.1409 46.8265 26.7943 52.9935C27.9209 57.5539 17.5428 72.245 45.95 72.597H56.2838C118.7 72.4536 181.116 72.5971 243.532 72.3102C341.083 72.0234 479.969 71.88 576.28 71.1629C520.891 70.876 465.502 70.7326 409.7 70.4458C298.508 70.1589 155.488 69.8721 45.5366 69.8721C24.7332 69.8721 49.4663 44.9935 21.0809 38.1766C15.7074 36.3122 8.68039 34.7346 0 34.5911V37.4595C9.5187 37.213 16.9866 35.8145 22.6721 33.7589C25.1301 32.8701 16.4888 32.2131 14.054 32.8701Z" fill="#9C7E21"/>
+            </svg>
           </div>
 
           <a href="#login" class="reg-success-cta-btn" id="btnBackToLogin" data-node-id="I1316:81060;708:582">
             <span class="reg-success-btn-text" data-node-id="I1316:81060;708:606">QUAY LẠI ĐĂNG NHẬP</span>
           </a>
 
+          <!-- Horror Metal Side Bracket Right -->
           <div class="reg-success-bracket-right">
-            <img src="./assets/2a8d91cd956a7b707d91ed3faed539513ed626cf.svg" alt="" />
+            <svg width="100%" height="100%" viewBox="0 0 576.28 72.597" fill="none" xmlns="http://www.w3.org/2000/svg" style="transform: scaleX(-1);">
+              <path d="M14.054 39.7269C10.3338 38.723 4.96024 38.1493 0.826722 38.0059V35.1375C21.081 34.1336 25.1409 25.7705 26.7943 19.6035C27.9209 15.0431 17.5428 0.35201 45.95 0H56.2838C118.7 0.143418 181.116 -4.38094e-06 243.532 0.286833C341.083 0.573669 479.969 0.717092 576.28 1.43418C520.891 1.72102 465.502 1.86444 409.7 2.15128C298.508 2.43811 155.488 2.72496 45.5366 2.72496C24.7332 2.72496 49.4663 27.6035 21.0809 34.4204C15.7074 36.2849 8.68039 37.8625 0 38.0059V35.1375C9.5187 35.384 16.9866 36.7825 22.6721 38.8382C25.1301 39.7269 16.4888 40.384 14.054 39.7269Z" fill="#9C7E21"/>
+              <path d="M14.054 32.8701C10.3338 33.8741 4.96024 34.4477 0.826722 34.5911V37.4595C21.081 38.4634 25.1409 46.8265 26.7943 52.9935C27.9209 57.5539 17.5428 72.245 45.95 72.597H56.2838C118.7 72.4536 181.116 72.5971 243.532 72.3102C341.083 72.0234 479.969 71.88 576.28 71.1629C520.891 70.876 465.502 70.7326 409.7 70.4458C298.508 70.1589 155.488 69.8721 45.5366 69.8721C24.7332 69.8721 49.4663 44.9935 21.0809 38.1766C15.7074 36.3122 8.68039 34.7346 0 34.5911V37.4595C9.5187 37.213 16.9866 35.8145 22.6721 33.7589C25.1301 32.8701 16.4888 32.2131 14.054 32.8701Z" fill="#9C7E21"/>
+            </svg>
           </div>
         </div>
 
         <!-- Divider Line (1316:81411) -->
         <div class="inbox-divider-row" data-node-id="1316:81411">
           <div class="inbox-divider-line">
-            <img src="./assets/e12ced11adff108c8b93e38961ee445224dbe44e.svg" alt="" />
+            <svg width="100%" height="2" viewBox="0 0 138 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="1" x2="138" y2="1" stroke="#9c7e21" stroke-width="1.5" stroke-dasharray="4 2"/>
+            </svg>
           </div>
           <span class="inbox-divider-text" data-node-id="1316:81061">Không nhận được email ?</span>
           <div class="inbox-divider-line">
-            <img src="./assets/e12ced11adff108c8b93e38961ee445224dbe44e.svg" alt="" />
+            <svg width="100%" height="2" viewBox="0 0 138 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="1" x2="138" y2="1" stroke="#9c7e21" stroke-width="1.5" stroke-dasharray="4 2"/>
+            </svg>
           </div>
         </div>
 
--- js/pages/forgot-password.js
+++ js/pages/forgot-password.js
@@ -10,7 +10,7 @@ export async function renderForgotPassword(container) {
       
       <!-- Background Grunge Overlay (1316:80882) -->
       <div class="reg-success-grunge-bg" data-node-id="1316:80882">
-        <img src="./assets/7c68e3ca72278b8193b62bcefa0dbacd65ab30a8.png" alt="" />
+        <img src="./assets/7c68e3ca72278b8193b62bcefa0dbacd65ab30a8.png" alt="" onerror="this.style.display='none'" />
       </div>
 
       <!-- Main Content Card (1316:80881) -->
@@ -34,24 +34,34 @@ export async function renderForgotPassword(container) {
             <div class="login-field-input-box" data-node-id="I1316:80791;1227:79708">
               <input type="email" id="forgotEmail" class="login-input-element" placeholder="user@gmail.com" required />
               <div class="reg-field-icon" data-node-id="I1316:80791;1231:2168">
-                <img src="./assets/7c2b03b2d6de7d18a9169a94ecdcb76923fdaa0e.svg" alt="" />
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#d9b74c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
+                </svg>
               </div>
             </div>
           </div>
 
           <!-- CTA Button: Gửi liên kết (1316:80810) -->
           <div class="reg-success-btn-frame" data-node-id="1316:80810">
-            <!-- Horror Metal Side Brackets -->
+            <!-- Horror Metal Side Bracket Left -->
             <div class="reg-success-bracket-left">
-              <img src="./assets/5330b79b9e3ec9f5d8e099674f3b9cd251731b0b.svg" alt="" />
+              <svg width="100%" height="100%" viewBox="0 0 576.28 72.597" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.054 39.7269C10.3338 38.723 4.96024 38.1493 0.826722 38.0059V35.1375C21.081 34.1336 25.1409 25.7705 26.7943 19.6035C27.9209 15.0431 17.5428 0.35201 45.95 0H56.2838C118.7 0.143418 181.116 -4.38094e-06 243.532 0.286833C341.083 0.573669 479.969 0.717092 576.28 1.43418C520.891 1.72102 465.502 1.86444 409.7 2.15128C298.508 2.43811 155.488 2.72496 45.5366 2.72496C24.7332 2.72496 49.4663 27.6035 21.0809 34.4204C15.7074 36.2849 8.68039 37.8625 0 38.0059V35.1375C9.5187 35.384 16.9866 36.7825 22.6721 38.8382C25.1301 39.7269 16.4888 40.384 14.054 39.7269Z" fill="#9C7E21"/>
+                <path d="M14.054 32.8701C10.3338 33.8741 4.96024 34.4477 0.826722 34.5911V37.4595C21.081 38.4634 25.1409 46.8265 26.7943 52.9935C27.9209 57.5539 17.5428 72.245 45.95 72.597H56.2838C118.7 72.4536 181.116 72.5971 243.532 72.3102C341.083 72.0234 479.969 71.88 576.28 71.1629C520.891 70.876 465.502 70.7326 409.7 70.4458C298.508 70.1589 155.488 69.8721 45.5366 69.8721C24.7332 69.8721 49.4663 44.9935 21.0809 38.1766C15.7074 36.3122 8.68039 34.7346 0 34.5911V37.4595C9.5187 37.213 16.9866 35.8145 22.6721 33.7589C25.1301 32.8701 16.4888 32.2131 14.054 32.8701Z" fill="#9C7E21"/>
+              </svg>
             </div>
 
             <button type="submit" class="reg-success-cta-btn" id="btnSendResetLink" data-node-id="I1316:80810;708:582">
               <span class="reg-success-btn-text" data-node-id="I1316:80810;708:606">GỬI LIÊN KẾT</span>
             </button>
 
+            <!-- Horror Metal Side Bracket Right -->
             <div class="reg-success-bracket-right">
-              <img src="./assets/2a8d91cd956a7b707d91ed3faed539513ed626cf.svg" alt="" />
+              <svg width="100%" height="100%" viewBox="0 0 576.28 72.597" fill="none" xmlns="http://www.w3.org/2000/svg" style="transform: scaleX(-1);">
+                <path d="M14.054 39.7269C10.3338 38.723 4.96024 38.1493 0.826722 38.0059V35.1375C21.081 34.1336 25.1409 25.7705 26.7943 19.6035C27.9209 15.0431 17.5428 0.35201 45.95 0H56.2838C118.7 0.143418 181.116 -4.38094e-06 243.532 0.286833C341.083 0.573669 479.969 0.717092 576.28 1.43418C520.891 1.72102 465.502 1.86444 409.7 2.15128C298.508 2.43811 155.488 2.72496 45.5366 2.72496C24.7332 2.72496 49.4663 27.6035 21.0809 34.4204C15.7074 36.2849 8.68039 37.8625 0 38.0059V35.1375C9.5187 35.384 16.9866 36.7825 22.6721 38.8382C25.1301 39.7269 16.4888 40.384 14.054 39.7269Z" fill="#9C7E21"/>
+                <path d="M14.054 32.8701C10.3338 33.8741 4.96024 34.4477 0.826722 34.5911V37.4595C21.081 38.4634 25.1409 46.8265 26.7943 52.9935C27.9209 57.5539 17.5428 72.245 45.95 72.597H56.2838C118.7 72.4536 181.116 72.5971 243.532 72.3102C341.083 72.0234 479.969 71.88 576.28 71.1629C520.891 70.876 465.502 70.7326 409.7 70.4458C298.508 70.1589 155.488 69.8721 45.5366 69.8721C24.7332 69.8721 49.4663 44.9935 21.0809 38.1766C15.7074 36.3122 8.68039 34.7346 0 34.5911V37.4595C9.5187 37.213 16.9866 35.8145 22.6721 33.7589C25.1301 32.8701 16.4888 32.2131 14.054 32.8701Z" fill="#9C7E21"/>
+              </svg>
             </div>
           </div>
 
